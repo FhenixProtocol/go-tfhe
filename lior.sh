deb5c78991117474dfcd24a41fbd77bf97db9911
Assaf Morami <assaf.morami@gmail.com>
2023-09-14 15:03:26 +0300

    rm start server from lior.sh

--- lior.sh
+++ lior.sh
@@ -9,4 +9,4 @@ mv build/rust-optimized.wasm build/rust.wasm
 wasm2wat build/rust.wasm |
  awk -F '[ ()]' '/banana/{banana=1; print; print "  (start "$8")"} {if(banana==1){banana=0}else{print}}' > build/rust.wat
 
-make start-web-server
+# make start-web-server
